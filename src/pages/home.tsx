c0f87183b0cd9dce96ec0837d28b3737bd5d9608
jrjlawas <jayromellawas30@gmail.com>
2025-12-06 16:45:17 +0800

    commented out the declared variables without used

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,13 @@
 import Header from "../components/header";
 import Hero from "../components/heros";
-import Categories from "../components/categories";
-import Promotions from "../components/promotions";
-import FoodGrid from "../components/foodGrid";
-import Process from "../components/Process";
+// import Categories from "../components/categories";
+// import Promotions from "../components/promotions";
+// import FoodGrid from "../components/foodGrid";
+// import Process from "../components/Process";
 import PopularFood from "../components/popularFood";
 import Testimonials from "../components/testimonials";
 import Faq from "../components/faq";
-import AppPart from "../components/app-part";
+// import AppPart from "../components/app-part";
 import Footer from "../components/footer";
 import logo from "../assets/images/logo/footer-logo.svg";
 
