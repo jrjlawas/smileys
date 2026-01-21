91d78de0ec06673810650afedbb63881aadae749
jrjlawas <jayromellawas30@gmail.com>
2026-01-21 09:49:00 +0800

    grid food(ispopular and category name added)

--- src/data/foodView.ts
+++ src/data/foodView.ts
@@ -1,10 +1,10 @@
 import img1 from "../assets/images/thumb/traditional-3.png";
-import img2 from "../assets/images/thumb/traditional-4.png";
-import img3 from "../assets/images/thumb/traditional-5.png";
-import img4 from "../assets/images/thumb/traditional-6.png";
-import img5 from "../assets/images/thumb/featured-1.png";
-import img6 from "../assets/images/thumb/featured-2.png";
-import img7 from "../assets/images/thumb/featured-3.png";
+// import img2 from "../assets/images/thumb/traditional-4.png";
+// import img3 from "../assets/images/thumb/traditional-5.png";
+// import img4 from "../assets/images/thumb/traditional-6.png";
+// import img5 from "../assets/images/thumb/featured-1.png";
+// import img6 from "../assets/images/thumb/featured-2.png";
+// import img7 from "../assets/images/thumb/featured-3.png";
 import pImg1 from "../assets/images/thumb/popular-1.png";
 import pImg2 from "../assets/images/thumb/popular-2.png";
 import pImg3 from "../assets/images/thumb/popular-3.png";
@@ -17,6 +17,8 @@ export interface gridFoodInt {
   id: number;
   title: string;
   img: string;
+  categoryName: String;
+  isPopular: string;
   items: string[];
   reviews: string;
   price: number;
@@ -38,84 +40,13 @@ export const gridFoods: gridFoodInt[] = [
     id: 1,
     title: "Pork Chop with Apple Chutney",
     img: img1,
+    categoryName: "",
+    isPopular: "string",
     items: ["4 Piece Chicken", "Spicy Sauce"],
     reviews: "4.7(2.5K)",
     price: 18.0,
     offer: "20% Off",
     tags: tagImg,
-  },
-  {
-    id: 2,
-    title: "Eggplant Parmesan & Linguine",
-    img: img2,
-    items: ["4 Piece Chicken", "Spicy Sauce"],
-    reviews: "4.7(2.5K)",
-    price: 40.0,
-    offer: "20% Off",
-  },
-  {
-    id: 3,
-    title: "Fish Tacos with Chipotle Crema",
-    img: img3,
-    items: ["4 Piece Chicken", "Spicy Sauce"],
-    reviews: "4.7(2.5K)",
-    price: 30.0,
-    offer: "20% Off",
-  },
-  {
-    id: 4,
-    title: "Quinoa-Stuffed Bell Pepper",
-    img: img4,
-    items: ["4 Piece Chicken", "Spicy Sauce"],
-    reviews: "4.7(2.5K)",
-    price: 30.0,
-    offer: "20% Off",
-  },
-  {
-    id: 5,
-    title: "Baked Chicken Wings and Legs",
-    img: img5,
-    items: ["4 Piece Chicken", "Spicy Sauce"],
-    reviews: "4.7(2.5K)",
-    price: 30.0,
-    offer: "20% Off",
-  },
-  {
-    id: 6,
-    title: "Fish Tacos with Chipotle Crema",
-    img: img3,
-    items: ["4 Piece Chicken", "Spicy Sauce"],
-    reviews: "4.7(2.5K)",
-    price: 30.0,
-    offer: "20% Off",
-  },
-  {
-    id: 7,
-    title: "Quinoa-Stuffed Bell Pepper",
-    img: img4,
-    items: ["4 Piece Chicken", "Spicy Sauce"],
-    reviews: "4.7(2.5K)",
-    price: 18.0,
-    offer: "20% Off",
-    tags: tagImg,
-  },
-  {
-    id: 8,
-    title: "Baked Chicken Wings and Legs",
-    img: img6,
-    items: ["4 Piece Chicken", "Spicy Sauce"],
-    reviews: "4.7(2.5K)",
-    price: 40.0,
-    offer: "20% Off",
-  },
-  {
-    id: 9,
-    title: "Fish Tacos with Chipotle Crema",
-    img: img7,
-    items: ["4 Piece Chicken", "Spicy Sauce"],
-    reviews: "4.7(2.5K)",
-    price: 30.0,
-    offer: "20% Off",
   },
 ];
 
